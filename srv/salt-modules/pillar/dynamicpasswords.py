96f1ba524b3efefe43764924e5ad4d2da74d9e4e
jdelic <jonas-bitbucket@maurus.net>
2015-09-12 15:46:06 +0200

    add base64 and length support and support for limited wildcard matching to dynamicpasswords and add encrpytion to consul

--- srv/salt-modules/pillar/dynamicpasswords.py
+++ srv/salt-modules/pillar/dynamicpasswords.py
@@ -89,23 +89,39 @@ class DynamicPasswordPillar(DynamicPasswordStore):
     def __init__(self, *args, **kwargs):
         super(DynamicPasswordPillar, self).__init__(*args, **kwargs)
 
-    def _pwcreate(self):
+    def _alphaencoding(self, rndstring):
         pwstr = ''.join([
                     str(self._PWDICT[(c & 0xf0) >> 4]) +  # c >= 64 are 2 chars
                     str(self._PWDICT[c & 0x0f])
                     if c >= 64 else self._PWDICT[c]  # c < 64 is 1 char
                     for c in [
-                        ord(b) for b in os.urandom(16)
+                        ord(b) for b in rndstring
                     ]
                 ])
         return pwstr
 
-    def create(self, username):
-        self[username] = self._pwcreate()
+    def create(self, username, encode=None, length=16):
+        if encode == 'base64':
+            self[username] = base64.b64encode(os.urandom(length))
+        else:
+            self[username] = self._alphaencoding(os.urandom(length))
 
 
 def ext_pillar(minion_id, pillar, *roledefs):
     db = DynamicPasswordPillar(_DEFAULT_PATH)
+
+    # (
+    #   {'*': [
+    #       {'consul-encryptionkey':
+    #           {'encode': 'base64'}
+    #       }]
+    #   },
+    #   {'database': [
+    #       'mysql-root',
+    #       'debian-sys-maint'
+    #   ]},
+    #   ...
+    # )
 
     # make sure all required users exist and filter the users
     # for the current minion's role
@@ -113,11 +129,32 @@ def ext_pillar(minion_id, pillar, *roledefs):
     for rd in roledefs:
         for r in rd:
             for u in rd[r]:
-                if u not in db:
+                username = None
+                encode = None
+                length = 16
+                if isinstance(u, dict):
+                    username = list(u.keys())[0]
+                    if 'encode' in u[username]:
+                        encode = u[username]['encode']
+                    if 'length' in u[username]:
+                        try:
+                            length = int(u[username]['length'])
+                        except ValueError:
+                            _log.error("Not a valid length specification: %s", u[username]['length'])
+                            continue
+                else:
+                    username = u
+
+                if username is None:
+                    _log.error("dynamicpasswords created None username for data %s in %s", u, r)
+                    continue
+
+                if username not in db:
                     _log.debug('creating dynamic password for %s', u)
-                    db.create(u)
-                if r in __grains__.get('roles', []):
-                    serverusers[u] = db[u]
+                    db.create(username, encode, length)
+
+                if r == '*' or r in __grains__.get('roles', []):  # '*' matches every node
+                    serverusers[username] = db[username]
 
     if 'roles' not in __grains__:
         return { 'dynamicpasswords': db.items() }
