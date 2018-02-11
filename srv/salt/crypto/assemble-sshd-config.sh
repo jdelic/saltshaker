f08a2a12ac5ffb513e477ee429c7f8920862e549
jdelic <jonas-bitbucket@maurus.net>
2018-02-12 00:56:30 +0100

    add a ssh config builder script so we can add sftp configs as necessary

---
+++ srv/salt/crypto/assemble-sshd-config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# concatenate sshd configs
+
+if [ -e /etc/ssh/sshd_config.new ]; then
+    rm /etc/ssh/sshd_config.new
+fi
+
+for cf in /etc/ssh/sshd_config.d/*; do
+    cat $cf >> /etc/ssh/sshd_config.new
+done
+
+# test new config
+if sshd -c /etc/ssh/sshd_config.new -t; then
+    mv /etc/ssh/sshd_config.new /etc/ssh/sshd_config
+    systemctl restart sshd
+else
+    echo "invalid config file detected"
+    exit 1
+fi
+
+exit 0
