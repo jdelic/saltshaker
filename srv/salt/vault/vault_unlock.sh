795e84436cb9413327d0a072afa6e91a17cb86ad
jdelic <jonas-github@maurus.net>
2020-02-05 00:31:16 +0100

    fix the vault unlock script

--- srv/salt/vault/vault_unlock.sh
+++ srv/salt/vault/vault_unlock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat /root/vault_keys.txt | grep "Unseal Key:" | cut -d' ' -f4 | head -3 | xargs -n 1 vault operator unseal
+grep "^Unseal Key" /root/vault_keys.txt | cut -d' ' -f4 | head -3 | xargs -n 1 vault operator unseal
