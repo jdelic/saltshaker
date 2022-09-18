a93988a974171ae29120e76d0c0201bfbb75bccb
jdelic <jonas-github@maurus.net>
2022-09-18 21:59:59 +0200

    putting together some terraform for tests

--- tf/hcloud/main.tf
+++ tf/hcloud/main.tf
@@ -7,3 +7,79 @@ provider "hcloud" {
     token = var.hcloud_token
 }
 
+resource "hcloud_server" "saltmaster" {
+    name        = "saltmaster"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
+
+resource "hcloud_server" "db" {
+    name        = "db"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
+
+resource "hcloud_server" "dev" {
+    name        = "dev"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
+
+resource "hcloud_server" "mail" {
+    name        = "mail"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
+
+resource "hcloud_server" "mail" {
+    name        = "mail"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
+
+resource "hcloud_server" "mail" {
+    name        = "mail"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
+
+resource "hcloud_server" "mail" {
+    name        = "mail"
+    image       = "debian-11"
+    server_type = ""
+
+    public_net {
+        ipv4_enabled = true
+        ipv6_enabled = true
+    }
+}
