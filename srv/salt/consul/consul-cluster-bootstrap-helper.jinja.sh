b1b520e35dc8ada99beecabb5b1bcd3c97e75ee5
Jonas Maurus <jonas-github@maurus.net>
2024-09-08 23:46:21 +0200

    check if script should be running and disable it after it did its job

--- srv/salt/consul/consul-cluster-bootstrap-helper.jinja.sh
+++ srv/salt/consul/consul-cluster-bootstrap-helper.jinja.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Exit if /etc/salt/roles.d/consulbootstrapprimary does not exist
+if [ ! -f /etc/salt/roles.d/consulbootstrapprimary ]; then
+    exit 0
+fi
+
 # Check the number of registered Consul servers using curl and jq
 CONSUL_SERVERS=$(curl -s http://127.0.0.1:8500/v1/agent/members | jq '[.[] | select(.Tags.role == "consul" and .Status == 1)] | length')
 
@@ -20,5 +25,11 @@ if [[ "$CONSUL_SERVERS" -ge {{target_number}} ]]; then
 
         # Restart the Consul service
         systemctl restart consul-server.service
+
+        # Disable the timer and service as they are no longer needed
+        systemctl stop consul-check.timer
+        systemctl disable consul-check.timer
+        systemctl disable consul-check.service
+        systemctl daemon-reload
     fi
 fi
